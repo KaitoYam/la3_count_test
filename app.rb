afbbf8518dd9966ac8cff6db5cf26b503f5d30b2
kaitoYam <k.yamaguchi.reiwaoo@gmail.com>
2022-02-14 05:45:49 +0000

    aaa

--- app.rb
+++ app.rb
@@ -8,3 +8,49 @@ before do
     Count.create(number: 0)
   end
 end
+
+get '/' do
+  @number= Count.first.number
+  erb :index
+end
+post '/plus' do
+  count = Count.first
+  count.number += 1
+  count.save
+  redirect'/'
+end
+
+post '/minus' do
+  count = Count.first
+  count.number -= 1
+  count.save
+  redirect'/'
+end
+
+post '/clear' do
+  count = Count.first
+  count.number = 0
+  count.save
+  redirect'/'
+end
+
+post '/x2' do
+  count = Count.first
+  count.number *= 2;
+  count.save
+  redirect'/'
+end
+
+post '/÷2' do
+  count = Count.first
+  count.number /= 2;
+  count.save
+  redirect'/'
+end
+
+# post '/plus2' do
+#   count = Count.first
+#   count.number2 += 1
+#   count.save
+#   redirect'/'
+# end
